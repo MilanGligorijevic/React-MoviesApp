8300a9edc35d18d1799029591583e14f309a4886
Milan Gligorijevic <milannnngligorijevic@gmail.com>
2024-03-26 19:00:29 +0100

    Initial commit
    Setting up structure for the project

---
+++ movies-app/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import Home from './views/pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
